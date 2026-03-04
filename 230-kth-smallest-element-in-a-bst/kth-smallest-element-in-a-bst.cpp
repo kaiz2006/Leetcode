0c5d2d099a49c46662737b370b1f1b62076b0656
Aaryan Kumar Thakur <thakuraaryan2006@gmail.com>
2026-03-04 10:32:01 +0530

    Time: 0 ms (100.00%) | Memory: 24.6 MB (17.08%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -24,7 +24,7 @@ public:
     int kthSmallest(TreeNode* root, int k) {
         vector<int> ans;
         inorder(root,ans);
-        sort(ans.begin(),ans.end());
+        
         return ans[k-1];
     }
 };
