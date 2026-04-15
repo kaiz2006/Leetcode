d62ff39a5a6b09e3d0d873ad648d09360c41c0bb
Aaryan Kumar Thakur <thakuraaryan2006@gmail.com>
2026-04-15 07:27:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (61.35%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
 
-    int solve(vector<int> &nums){
-    
-        int n = nums.size();
-        if(n==1){
-            return nums[0];
-        }
-        if(n==0){
-            return 0;
-        }
+    int solve(vector<int>& nums, int start, int end) {
         
+        if(start == end) return nums[start];
 
-        int prev2 = nums[0];
-        int prev1 = max(nums[0], nums[1]);
+        int prev2 = nums[start];
+        int prev1 = max(nums[start], nums[start + 1]);
 
-        for(int i = 2; i < n; i++){
+        for(int i = start + 2; i <= end; i++){
             int incl = prev2 + nums[i];
             int excl = prev1;
             int ans = max(incl, excl);
@@ -28,18 +21,16 @@ public:
     }
 
     int rob(vector<int>& nums) {
-        
         int n = nums.size();
+
         if(n == 1) return nums[0];
 
-        vector<int> nums1(nums.begin(), nums.end() - 1);
-        vector<int> nums2(nums.begin() + 1, nums.end());
+        
+        int a = solve(nums, 0, n - 2);
 
         
+        int b = solve(nums, 1, n - 1);
 
-        int a =  solve(nums1);
-        int b= solve(nums2);
-
-        return max(a,b);
+        return max(a, b);
     }
 };
