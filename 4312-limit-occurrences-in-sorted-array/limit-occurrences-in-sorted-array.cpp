9d5b963a543ab2f9a1c4b55385b6adfbabbd7440
Aaryan Kumar Thakur <thakuraaryan2006@gmail.com>
2026-06-09 22:00:29 +0530

    Time: 3 ms (37.45%) | Memory: 33.7 MB (38.81%) - LeetSync

---
+++ 4312-limit-occurrences-in-sorted-array/limit-occurrences-in-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> limitOccurrences(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        vector<int> ans;
+
+        for (int x : nums) {
+            if (mp[x] < k) {
+                ans.push_back(x);
+                mp[x]++;
+            }
+        }
+
+        return ans;
+    }
+};
