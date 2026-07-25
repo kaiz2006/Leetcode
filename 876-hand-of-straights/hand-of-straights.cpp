8314a091f156222fb425cf56958e999626f48be9
Aaryan Kumar Thakur <thakuraaryan2006@gmail.com>
2026-07-25 18:26:00 +0530

    Time: 47 ms (20.22%) | Memory: 32.3 MB (42.20%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if(hand.size()%groupSize !=0) return false;
+        map<int,int> mp;
+        for(auto &it : hand){
+            mp[it]++;
+        }
+        
+        for(auto &it :mp){
+            int start = it.first;
+            int freq = it.second;
+
+            if (freq == 0) continue;
+
+            
+            for (int i = 0; i < groupSize; i++) {
+                if (mp[start + i] < freq)
+                    return false;
+
+                mp[start + i] -= freq;
+            }
+        }
+        return true;
+
+    }
+};
