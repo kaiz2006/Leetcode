f988f7283a8c82b5a36dbb1e84f4f554ead4976a
Aaryan Kumar Thakur <thakuraaryan2006@gmail.com>
2026-07-27 19:45:53 +0530

    Time: 7 ms (75.31%) | Memory: 192.4 MB (85.06%) - LeetSync

---
+++ 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int smallestBalancedIndex(vector<int>& nums) {
+        unsigned long long lsum = 0;
+        for (int x : nums) lsum += x;
+
+        unsigned long long rprod = 1;
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            lsum -= nums[i];
+
+            if (lsum == rprod)
+                return i;
+
+            if (rprod > lsum / nums[i])
+                break;
+
+            rprod *= nums[i];
+        }
+
+        return -1;
+    }
+};
