2d7de87936b535767c2f8bba4054066dda320d68
Aaryan Kumar Thakur <thakuraaryan2006@gmail.com>
2026-09-12 16:56:47 +0530

    Time: 8 ms (55.98%) | Memory: 20 MB (98.99%) - LeetSync

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        vector<int> freq(26,0);
+
+        for(auto c : s){
+            freq[c-'a']++;
+        }
+        sort(freq.begin(),freq.end());
+        int ans = 0;
+        for(int i =24;i>=0;i--){
+            if(freq[i] == 0){
+                break;
+            }
+            if(freq[i] >= freq[i+1]){
+                int prev=freq[i];
+                freq[i] = max(0,freq[i+1]-1);
+                ans+=prev-freq[i];
+            }
+        }
+        return ans;
+    }
+};
